fef2adc180c7991e836146915596fc995c8cf9cd
duouoduo <2080124045@qq.com>
2023-04-01 13:44:06 +0800

    重写了计算部分，使代码更好读一些。

--- app.py
+++ app.py
@@ -1,31 +1,46 @@
 import datetime
 import time
-STAMINA_MAX = 160
-STAMINA_RECOVER_TIME = 480
+# 最大树脂
+RESIN_MAX = 160
+# 树脂恢复时间
+RESIN_RECOVER_TIME = 1
 
-# 体力计算
-def stamina_calculate():
+# 剩余树脂计算
+def remain_resin_(current_resin):
+    return RESIN_MAX - current_resin
+# 剩余时间计算
+def remain_time_(remain_resin):
+    return datetime.timedelta(seconds=remain_resin * RESIN_RECOVER_TIME)
+# 计算剩余树脂&剩余时间
+def calculate(current_resin):
+    remain_resin = remain_resin_(current_resin)
+    remain_time = remain_time_(remain_resin)
+    return remain_resin, remain_time
+# 数据输入
+def inputs():
     pass
 # 主程序
 def app():
-    
-    current_stamina_str = input('请输入当前体力值：')
-    current_stamina = int(current_stamina_str)if current_stamina_str else 0
-    remain_time = datetime.timedelta(seconds=(STAMINA_MAX - current_stamina) * STAMINA_RECOVER_TIME)
-    object_time = datetime.datetime.now() + remain_time
-    print('体力计算器')
+    # 获取当前树脂
+    current_resin_str = input('请输入当前体力值：')
+    current_resin = int(current_resin_str)if current_resin_str else 0
+    data = calculate(current_resin)
+    # 计算剩余时间
+    remain_time = remain_time_(data[0])
+    # 当前时间&回满时间
     current_time = datetime.datetime.now()
+    object_time = datetime.datetime.now() + remain_time
+    print('树脂计算器')
     print('当前时间：', current_time.strftime('%Y-%m-%d %H:%M:%S'))
     print('回满时间：', object_time.strftime('%Y-%m-%d %H:%M:%S'))
-    reamain_time = (STAMINA_MAX - current_stamina)*STAMINA_RECOVER_TIME
     while True:
-        print('体力面板：{}/{},剩余时间{}/{}'.format(current_stamina, STAMINA_MAX, remain_time,object_time.strftime('%H:%M:%S')),end='\r')
-        if current_stamina == STAMINA_MAX:
-            print('体力已于{}回满，开原！'.format(current_time))
+        print('体力面板：{}/{},剩余时间：{}/{}'.format(current_resin, RESIN_MAX, remain_time,object_time.strftime('%H:%M:%S')),end='\r')
+        if current_resin == RESIN_MAX:
+            print('体力已于{}回满，开原！'.format(current_time.strftime('%H:%M:%S')))
             break
         time.sleep(1)
         remain_time = remain_time - datetime.timedelta(seconds=1)
-        if int(remain_time.total_seconds())%STAMINA_RECOVER_TIME == 0:
-            current_stamina += 1
+        if int(remain_time.total_seconds())%RESIN_RECOVER_TIME == 0:
+            current_resin += 1
 if __name__ == '__main__':
     app()
